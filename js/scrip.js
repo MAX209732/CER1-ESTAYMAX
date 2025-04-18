30465c2035b15d4795f71dbe7e4cfa146fe063ac
MAX209732 <maxi.estay.cu@gmail.com>
2025-04-17 20:51:22 -0400

    Add files via upload

---
+++ js/scrip.js
@@ -0,0 +1,65 @@
+function saludo(){
+    alert("hecho por Maximiliano Estay");
+}
+function Verde(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'green'
+    })
+}
+function azul(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'blue'
+    })
+}
+function Gris(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'grey'
+    })
+}
+function Rojo(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'red'
+    })
+}
+function Amarillo(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'yellow'
+    })
+}
+function Celeste(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'skyblue'
+    })
+}
+function Blanco(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'white'
+    })
+}
+function Negro(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = 'Black'
+    })
+}
+function Defecto(){
+    const color=document.getElementById('cuerpo')
+    color.addEventListener('click',function(){
+        color.style.background = '#9d9257'
+    })
+}
+function Inss(){
+    alert("USM");
+}
+function Rutina(){
+    alert("Desayuño: 2 huevos 4 laminas de pan")
+    alert ("ALmuerzo: 200 ml de arroz con 100 gr de pollo o 4 laminas de pan 1 lamina de queso y 2 laminas de jamon pavo soprava")
+    alert("lcena: lo que no preparaste en el almuerzo");
+}
